f32946e66e7f425da0e77341e5e019364d49bda8
Steven Wong <syw14@sfu.ca>
2023-04-06 16:47:54 +0800

    take input from cmld

---
+++ programming-assignments/PA2/src/maxPlanarSubset.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+// For now
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+    // Ensure that the correct number of arguments are given
+    if (argc != 3)
+    {
+        cout << "Usage: ./mps <input_filename> <output_filename>" << endl;
+        return 1;
+    }
+
+
+
+    return 0;
+}
