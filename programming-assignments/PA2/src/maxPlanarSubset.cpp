391681a6304a5f34e19f7cec575f0b7bf7af951f
Steven Wong <syw14@sfu.ca>
2023-04-19 17:16:25 +0800

    update file reading

--- programming-assignments/PA2/src/maxPlanarSubset.cpp
+++ programming-assignments/PA2/src/maxPlanarSubset.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <set>
 #include <fstream>
+#include <iostream>
+#include <iomanip>
 #include <vector>
 
 using namespace std;
@@ -22,38 +22,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Declare our variables for calculating the maximal planar set
+    int vecticeCount = 0;
+    vector<int> chords;
+    vector<vector<int> > matrices;
+    vector<bool> maxChords;
+
     // Input file contains an integer 2n, followed by n lines, 
     // each containing 2 integers denoting two endpoints of an chord
     // A single '0' denotes the end of the input
 
-    // Read in the number of chords
-    int n;
-    input >> n;
-    vector<pair<int, int> > chords;
-
-    // While there are still chords to read in
-    for (int i = 0; i < n; i++)
-    {
-        int x, y;
-        input >> x >> y;
-        chords.push_back(make_pair(x, y));
-    }
-
-    // Print out the chords
-    cout << "Chords:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << chords[i].first << " " << chords[i].second << endl;
-    }
-
-    // Assumption: No chords of C share an endpoint, let M(i,j) , i <= j denote the number of non-overlapped chords in the maximum planar
-    // subset in the region formed by chord ij and the arc between endpoints i and j
-    //
-    // Compute M(0, 2n-1) and report the subset sorted by the first endpont in ascending order
 
 
-    // Note to self: Selection of planar subset given by formula (nCk)/n!nCk
-    // Ie we havethe rleation shi
 
     // Close the input file
     input.close();
